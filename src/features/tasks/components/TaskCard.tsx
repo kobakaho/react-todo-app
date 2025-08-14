99855eda756d2f44604a2f0a82d722147d6a8c62
kobakaho <kapotast@gmail.com>
2025-08-14 17:05:55 +0900

    切出した関数をimport

--- src/features/tasks/components/TaskCard.tsx
+++ src/features/tasks/components/TaskCard.tsx
@@ -1,33 +1,15 @@
 import { Link } from "react-router-dom";
-import { Priority, Task } from "../../../types/task";
+// import { Priority, Task } from "../../../types/task"; 
 import styles from "../styles/taskCard.module.css";
+import { getPriorityClass } from "../utils/priority";
+import { getStatusLabel } from "../utils/status-label";
+import { Task } from "../../../types/task";
 
 type Props = {
     task: Task;
 };
 
 export default function TaskCard({ task }: Props) {
-
-    // タスクの優先度に応じたクラス名・スタイルを返す関数
-    // Priority TSで定義した型
-    const getPriorityClass = (priority: Priority): string => {
-        switch (priority.toLowerCase()) {
-            case "high":
-                return styles.highPriority
-            case "midium":
-                return styles.midiumPriority
-            case "low":
-                return styles.lowPriority
-            default:
-                return styles.defaultPriority
-        }
-    }
-
-    // タスクのステータスに応じた表示ラベル・スタイルを返す関数
-    const getStatusLabel = (status: boolean): string => {
-        return status ? "完了" : "未完了";
-    };
-
     return (
       <div className={styles.taskCard}>
         <Link to={`/tasks/${task.id}`} className={styles.detailLink}>
@@ -36,11 +18,10 @@ export default function TaskCard({ task }: Props) {
         <p className={`${styles.badge} ${task.status ? styles.completed : styles.incomplete}`}>
           {getStatusLabel(task.status)}
         </p>
-        <p className={`${styles.badge} ${getPriorityClass(task.priority)}`}>{task.priority}</p>
+        <p className={`${styles.badge} ${getPriorityClass(task.priority, styles)}`}>{task.priority}</p>
         <p className={styles.taskDueDate}>{task.dueDate}</p>
       </div>
     );
 }
-// タスクオステータスに応じてスタイルを適用
-// <Link to={`/tasks/${task.id}`}>詳細</Link>
+// タスクのステータスに応じてスタイルを適用
 //　可読性を向上するためのコンポーネント分割
