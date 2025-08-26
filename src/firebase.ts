851d23fe245769da87fe5f89ef15b1df07a46524
kobakaho <kapotast@gmail.com>
2025-08-27 01:18:09 +0900

    拡張子変更

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getDatabase(app);
+const db = getFirestore(app);
+
+export { db, analytics };
