e591843f7e24730c82307b14866a1b1ddabc02dc
kobakaho <kapotast@gmail.com>
2025-08-27 17:52:39 +0900

    相対パス修正

--- src/features/auth/SignUp.tsx
+++ src/features/auth/SignUp.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from "./firebase"; // authインスタンスをインポート
+import { auth } from "../../firebase"; // authインスタンスをインポート
 import { useNavigate } from 'react-router-dom';
 
 const SignUp: React.FC = () => {
