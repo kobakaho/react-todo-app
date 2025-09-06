86dfcd904182931addc9e501a5370f9d1d620972
kobakaho <kapotast@gmail.com>
2025-09-06 14:06:03 +0900

    propsを渡す

--- src/pages/tasks/taskEditPage.tsx
+++ src/pages/tasks/taskEditPage.tsx
@@ -1,9 +1,21 @@
 import TaskEditFormContainer from "../../features/tasks/containers/TaskEditFormContainer";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function TaskEditPage() {
+  const [open, setOpen] = useState(true);
+  const { id } = useParams<{ id?: string }>(); //編集時にはidが必要なためidを渡す
+
+  if (!id) {
+    return null;
+  }
   return (
     <div>
-      <TaskEditFormContainer />
+      <TaskEditFormContainer
+        open={open}
+        onClose={() => setOpen(false)}
+        id={id}
+      />
     </div>
   )
 }
